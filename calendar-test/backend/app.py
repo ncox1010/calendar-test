a7b442f7a197ef396a82e76586695fbe7075ad48
unknown <ncox36@gatech.edu>
2024-05-15 17:53:37 -0400

    4-functional-proto

--- calendar-test/backend/app.py
+++ calendar-test/backend/app.py
@@ -2,7 +2,9 @@ from flask import Flask # type: ignore
 import flask
 import json
 import csv
+from datetime import datetime
 from flask_cors import CORS
+import re
 
 
 app = Flask(__name__)
@@ -12,19 +14,192 @@ CORS(app)
 def hello():
     return "Hello, World!"
 
-@app.route('/Sampledata', methods=["GET"])
-def Sampledata():
+@app.route('/Sampledata/table', methods=["GET"])
+def SampledataTable():
     with open('Sampledata.csv', 'r') as file:
         reader = csv.reader(file)
-        data = list(reader)  # Read all rows and store them in a list
-        return flask.jsonify(data)
+        data = list(reader)
+        del data[0]
+        json_grid = []
+        for r in data:
+            if r:
+                cal = False
+                wat = False
+                book = False
+                if (r[8] == "Add to calendar"):
+                    cal = True
+                if (r[8] == "Wishlist"):
+                    wat = True
+                if (r[8] == "Bookmark"):
+                    book = True
+                json_row = {
+                    "Title":r[0],
+                    "Calendar": cal,
+                    "Watchlist": wat,
+                    "Bookmarked": book,
+                    "Event Category": r[11]
+                }
+                json_grid.append(json_row)
+    return json_grid
+
+@app.route('/Sampledata/events', methods=["GET"])
+def Sampledata():
+    json_events = []
+    with open('Sampledata.csv', 'r') as file:
+        reader = csv.reader(file)
+        #print (reader)
+        data = list(reader)
+        del data[0]
+        for r in data:
+            if r:  # Check if the row is not empty
+                json_event = {
+                    "title": r[0],
+                    "start": "",  # Assuming the date is in the 7th column
+                    "end": "",
+                    "color": ""
+                }
+                # Create a new dictionary for each event
+                date_str = r[6].strip("[]")  # Remove the square brackets
+                date_list = [date.strip().strip("'") for date in date_str.split("' ,'")]
+                for d in date_list:
+                    if ("', '" in d):
+                        date_list.remove(d)
+                        date_list.extend(d.split("', '"))
+                date_list = sorted(date_list)
+                if len(date_list) == 1:
+                    json_event["start"] = dateFormat(date_list[0])
+                else:
+                    json_event["start"] = dateFormat(date_list[0])
+                    json_event["end"] = dateFormat(date_list[-1])
+                if r[11] == 'Family':
+                    json_event["color"] = "red"
+                elif r[11] == 'Exhibit':
+                    json_event["color"] = "blue"
+                else:
+                    json_event["color"] = "green"
+                json_events.append(json_event)
+        return (json_events)
     
-@app.route('/Parserfile', methods=["GET"])
-def Parsefile():
+def dateFormat (date):
+    date_str = ""
+    date_li = date.split(" ")
+    if (len(date_li) != 3):
+        return "Invalid date"
+    month = date_li[0]
+    day = date_li[1]
+    if (',' in day):
+        day = day.strip(',')
+    year = date_li[2]
+    if (',' in year):
+        year = year.strip(',')
+    date_str += year
+    if (month.lower() == "january" or month.lower() == "jan"):
+        date_str += "-01-"
+    elif (month.lower() == "february" or month.lower() == "feb"):
+        date_str += "-02-"
+    elif (month.lower() == "march" or month.lower() == "mar"):
+        date_str += "-03-"
+    elif (month.lower() == "april"or month.lower() == "apr"):
+        date_str += "-04-"
+    elif (month.lower() == "may"):
+        date_str += "-05-"
+    elif (month.lower() == "june" or month.lower() == "jun"):
+        date_str += "-06-"
+    elif (month.lower() == "july" or month.lower() == "jul"):
+        date_str += "-07-"
+    elif (month.lower() == "august" or month.lower() == "aug"):
+        date_str += "-08-"
+    elif (month.lower() == "september" or month.lower() == "sep"):
+        date_str += "-09-"
+    elif (month.lower() == "october" or month.lower() == "oct"):
+        date_str += "-10-"
+    elif (month.lower() == "november" or month.lower() == "nov"):
+        date_str += "-11-"
+    elif (month.lower() == "december" or month.lower() == "dec"):
+        date_str += "-12-"
+
+    if len(day) == 1:
+        date_str += "0"
+        date_str += day
+    else:
+        date_str += day
+
+    return date_str
+        
+
+
+    
+@app.route('/Parserfile/events', methods=["GET"])
+def Parserfile():
+    json_events = []
     with open('Parserfile.csv', 'r') as file:
         reader = csv.reader(file)
-        data = list(reader)  # Read all rows and store them in a list
-        return flask.jsonify(data)
-
+        #print (reader)
+        data = list(reader)
+        del data[0]
+        for r in data:
+            if r:  # Check if the row is not empty
+                json_event = {
+                    "title": r[0],
+                    "start": "",  # Assuming the date is in the 7th column
+                    "end": "",
+                    "color": ""
+                }
+                # Create a new dictionary for each event
+                if (r[6] == "null"):
+                    date_components = r[7].split(' ')
+                    date_re = ''
+                    for d in date_components[0:3]:
+                        date_re += d
+                        date_re += " "
+                    print(date_re.strip(' '))
+                    json_event["start"] = dateFormat(date_re.strip(' '))
+                    #print(json_event)
+                else:     
+                    date_str = r[6].strip("[]")  # Remove the square brackets
+                    date_list = [date.strip().strip("'") for date in date_str.split("-")]
+                
+                    if len(date_list) == 1:
+                        json_event["start"] = dateFormat(date_list[0])
+                    else:
+                        json_event["start"] = dateFormat(date_list[0])
+                        json_event["end"] = dateFormat(date_list[-1])
+                if r[12] == 'Family':
+                    json_event["color"] = "red"
+                elif r[12] == 'Exhibit':
+                    json_event["color"] = "blue"
+                else:
+                    json_event["color"] = "green"
+                json_events.append(json_event)
+        return (json_events)
+    
+@app.route('/Parserfile/table', methods=["GET"])
+def ParserfileTable():
+    with open('Parserfile.csv', 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+        del data[0]
+        json_grid = []
+        for r in data:
+            if r:
+                cal = False
+                wat = False
+                book = False
+                if (r[8] == "Add to Calendar"):
+                    cal = True
+                if (r[8] == "Wishlist"):
+                    wat = True
+                if (r[8] == "Bookmark"):
+                    book = True
+                json_row = {
+                    "Title":r[0],
+                    "Calendar": cal,
+                    "Watchlist": wat,
+                    "Bookmarked": book,
+                    "Event Category": r[12]
+                }
+                json_grid.append(json_row)
+    return json_grid
+    
 if __name__ == "__main__":
     app.run("localhost", 6969)
--- calendar-test/src/App.js
+++ calendar-test/src/App.js
@@ -1,18 +1,85 @@
 import './App.css';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import axios from axios;
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+
 
 
 function App() {
 
+  function callDataParser() {
+      axios.get('http://localhost:6969/Parserfile/table')
+        .then(function (response){
+          console.log(response.data)
+          setRowData(response.data)
+        });
+  
+      axios.get('http://localhost:6969/Parserfile/events')
+        .then(function (response){
+          setevents_from_data(response.data)
+      });
+  }
+
+  function callDataSample() {
+      axios.get('http://localhost:6969/Sampledata/table')
+        .then(function (response){
+          console.log(response.data)
+          setRowData(response.data)
+        });
+  
+      axios.get('http://localhost:6969/Sampledata/events')
+        .then(function (response){
+          setevents_from_data(response.data)
+      });
+  }
+
+  const [events_from_data, setevents_from_data] = useState([]);
+  const [colDefs, setColDefs] = useState([
+    { field: "Title"},
+    { field: "Calendar"},
+    { field: "Watchlist"},
+    { field: "Bookmarked"},
+    { field: "Event Category" }
+  ]);
+
+  const [rowData, setRowData] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:6969/Sampledata/table')
+      .then(function (response){
+        console.log(response.data)
+        setRowData(response.data)
+      });
+
+    axios.get('http://localhost:6969/Sampledata/events')
+      .then(function (response){
+        setevents_from_data(response.data)
+    });
+  }, [])
 
   return (
+    <div>
+      <div>
+        <button onClick={callDataParser}>Parser File</button>
+        <button onClick={callDataSample}>Sample Data</button> 
+      </div>
     <div className="App">
       <FullCalendar
         plugins={[ dayGridPlugin ]}
         initialView="dayGridMonth"
+        events={events_from_data}
       />
+    </div>
+    <div style={{ height: 2000 }} className="ag-theme-quartz">
+      <AgGridReact 
+       columnDefs={colDefs}
+       rowData={rowData}
+       rowSelection="multiple"
+   />
+    </div>
     </div>
     
   );
